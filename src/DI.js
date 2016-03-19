4341da689467fc9822325bd2a718144594f882fb
howard.zuo <leftstick@qq.com>
2016-03-19 20:53:48 +0800

    update impl

--- src/DI.js
+++ src/DI.js
@@ -11,17 +11,21 @@ DI.prototype.register = function(name, inst) {
 DI.prototype.run = function(arr) {
     var cb,
         argsName,
-        args;
+        args,
+        lastIndex = arr.length - 1;
     if (Array.isArray(arr)) {
-        cb = arr[arr.length - 1];
-        argsName = arr.slice(0, arr.length - 1);
+        cb = arr[lastIndex];
+        argsName = arr.slice(0, lastIndex);
     } else if (arr.$inject) {
         cb = arr;
         argsName = arr.$inject;
     } else {
         cb = arr;
-        var cbStr = arr.toString();
-        argsName = cbStr.match(/\(\s*([^\)]*)\)/)[1].split(',').map(name => name.trim());
+        argsName = arr
+            .toString()
+            .match(/\(\s*([a-zA-Z,\s]*)\)/)[1]
+            .split(',')
+            .map(name => name.trim());
     }
     args = argsName.map(name => this.store[name]);
     cb.apply(null, args);
